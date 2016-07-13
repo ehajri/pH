11cc6f7fd909262827602e2fcd16a11c6bfd1f0b
Ebrahem Al-Hajeri <ebrahem.alhajri@gmail.com>
2016-07-13 15:46:41 +0300

    cleaned old code and applied renderable on sub menus

--- src/ph.js
+++ src/ph.js
@@ -16,9 +16,6 @@
     // R.filter(x => isRoot(x), objects);
     // R.filter(x => Renderable(x), objects);
 
-
-    var sections = Object.create(null);
-
     var parentmenu = {
         view: function(ctrl, args) {
             return (
@@ -34,27 +31,9 @@
     var submenu = {
         view: function(ctrl, subs) {
             return (
-            m('ul', {class: 'dropdown-menu pull-left'},
-                [
-                    subs.map(function(sub) {return sub.renderAsMenu === false ? null : (
-                        m('li',
-                            m('a', {
-                                href: sub.route,
-                                config: m.route
-                            }
-                            , m('i.fa.fa-cubes')
-                            , ' ' + sub.header)
-                        ));})
-            ])
-            );
-        }
-    };
-    submenu = {
-        view: function(ctrl, subs) {
-            return (
                 m('ul', {class: 'dropdown-menu pull-left'},
                     [
-                        subs.map(function(sub) {
+                        R.filter(x => renderable(x), subs).map(function(sub) {
                             return (
                             m('li',
                                 m('a', {
@@ -68,6 +47,7 @@
             );
         }
     };
+
     var lonemenu = {
         view: function(ctrl, args) {
             return (
@@ -80,6 +60,7 @@
             );
         }
     };
+
     var dropdownmenu = {
       view: function(ctrl, args) {
           return (
@@ -92,29 +73,7 @@
       }
     };
 
-
-
-
-
-
-
     var nav = {
-        view: function() {
-            return (
-            R.keys(sections).map(function(section) {
-                var sec = sections[section];
-                if (!authorized(_claims, sec)) { return; }
-                return (
-                    R.has('sub')(sec) && sec.sub.length > 0 ?
-                    m.component(dropdownmenu, {header: sec.header, sub: sec.sub})
-                    :
-                    m.component(lonemenu, {header: sec.header, route: sec.route || ''})
-                );
-            }));
-        }
-    };
-
-    nav = {
         view: function() {
             return (
                 sorted(roots(_sections)).map(function(section) {
@@ -141,43 +100,15 @@
     };
 
     var Section = function(section) {
-        if (R.equals(R.type(section), 'Object')) {
-            var id = this.id = section.id || section.header;
-            if (!R.has(id)(sections)) {
-                //copy properties over to this
-                Object.assign(this, section);
-                sections[id] = this;
-            } else {
-                throw new Error({name: 'Section Error', message: 'Section "' + id + '" does exist!'});
-            }
-        } else {
-            if (!R.has(section)(sections)) {
-                sections[section] = this;
-            } else {
-                throw new Error({name: 'Section Error', message: 'Section "' + section + '" does exist!'});
-            }
-            this.id = this.header = section;
-        }
-    };
-
-    var Section2 = function(section) {
         "use strict";
         this.id = guid();
         Object.assign(this, section);
         _sections.push(this);
     }
-    Section2.prototype.AddModule = function (module) {
+
+	Section.prototype.AddModule = function (module) {
         module.parent = this.id;
         _sections.push(module);
-        return this;
-    };
-
-    Section.prototype.AddModule = function (module) {
-        if (!R.has('sub')(this)) {
-            this['sub'] = [];
-        }
-
-        this['sub'].push(module);
         return this;
     };
 
@@ -204,23 +135,6 @@
     };
 
     var authorized = function(claims, component) {
-        console.log('checking claims for', component.header, 'user', claims);
-
-        var required = component.claims,
-            requiredaslist = R.equals(R.type(required), 'Object') ? [required] : required;
-        var bool = (R.contains({type: 'Type', value: 'SystemAdministrator'}, claims) ||
-            (R.has('claims')(component) && (
-                R.equals({type: 'Type', value: 'any'}, required) ||
-
-                R.intersection(claims, requiredaslist).length > 0
-            )));
-        console.log(bool);
-        return bool;
-    };
-
-    authorized = function(claims, component) {
-        console.log(component);
-
         var required = component.claims,
             requiredaslist = R.equals(R.type(required), 'Object') ? [required] : required;
         var bool = (R.contains({type: 'Type', value: 'SystemAdministrator'}, claims) ||
@@ -229,57 +143,31 @@
 
             R.intersection(claims, requiredaslist).length > 0
         )));
-        console.log(bool);
         return bool;
     };
 
     var GetRoutes = function() {
-        console.log('claims', _claims);
-
-        return R.reduce(
-            (object, value) => {
-                "use strict";
-                var tempObj = {};
-                tempObj[value['route']] = value;
-                return R.merge(object, tempObj);
-            },
-            {},
-            filter(_claims)
-        );
-    };
-
-    GetRoutes = function() {
         "use strict";
         return R.fromPairs(R.filter(x => R.has('route')(x), _sections).map(x => [x.route, x.section]));
     }
 
     var _claims = [];
 
-
-    document.addEventListener('DOMContentLoaded', function(e) {
-        //m.mount(document.getElementById'main'), AccModule1);
-        //m.mount(document.getElementById('nav'), nav);
-        //m.route(document.getElementById('main'), '/', GetRoutes());
-    });
-
     var pHObj = {
         SetClaims: function(claims) {
             _claims = claims;
         },
         filter: filter,
-        sections: sections,
-        sections2: _sections,
+        sections: _sections,
         Routes: GetRoutes(),
         AddSection: function(section) {
-            return new Section2(section);
+            return new Section(section);
         },
         InitMenu: function(node) {
             m.mount(document.getElementById(node), nav);
         },
         InitRoute: function(node) {
             var rr = GetRoutes();
-            console.log('setting routes, total of', R.keys(rr).length)
-            console.log('routes', rr);
             m.route(document.getElementById(node), '/', rr);
         },
         From: function(section) {
